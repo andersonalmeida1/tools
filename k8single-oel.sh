bfdea5ea331924fb1bf580ea1fbd9afbc3f1dbdc
Anderson de Almeida <70177495+andersonalmeida1@users.noreply.github.com>
2020-09-13 09:55:38 -0300

    Update k8single-oel.sh

--- k8single-oel.sh
+++ k8single-oel.sh
@@ -1,4 +1,4 @@
-# v2.4
+# v3
 # Install K8 single node on Ubuntu
 # !!!!!! Rodar em oel7.8 !!!!!!!!
 # https://enabling-cloud.github.io/oci-learning/manual/KubernetesClusterOnOCI.html
@@ -7,23 +7,27 @@
 # com OKE
 #  
 # 1 install docker---------------------------------
+if [[ $1 == '1' ]]; then
 sudo yum -y update
 sudo yum -y install docker-engine
 sudo systemctl daemon-reload
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo docker version
+fi
 
 # 1.1 SETUP ------------------------------------
 # Set SELinux in permissive mode (effectively disabling it)
+if [[ $1 == '11' ]]; then
 sudo setenforce 0
 sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 sudo sed -i 's/^SELINUXTYPE=mls$/SELINUXTYPE=targeted/' /etc/selinux/config
 sudo systemctl disable firewalld && sudo systemctl stop firewalld
-
+fi
 
 # 2 install k8 ---------------------------------
 # @@@@@@@@@@@@ FAZER MANUAL @@@@@@@@@@@@@@2/
+if [[ $1 == '2' ]]; then
 sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -38,22 +42,30 @@ sudo cat <<EOF >  /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
+fi
+if [[ $1 == '22' ]]; then
 sudo sysctl --system
 sudo yum install -y  kubelet kubeadm kubectl kubernetes-cni
 sudo systemctl enable kubelet && sudo systemctl start kubelet
 #disable swap
 sudo swapoff -a &&  sudo sed -i '/ swap / s/^/#/' /etc/fstab
+fi
 # 3 init cluster--------------------------------
+if [[ $1 == '3' ]]; then
 sudo kubeadm reset -f && sudo rm -rf /etc/kubernetes/
 sudo kubeadm init
-sudo export KUBECONFIG=/etc/kubernetes/admin.conf
+export KUBECONFIG=/etc/kubernetes/admin.conf
 sudo mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+fi
 # 4 setup pod net--------------------------------
+if [[ $1 == '4' ]]; then
 sudo kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl  version | base64 | tr -d '\n')"
 sudo kubectl get pods --all-namespaces
+fi
 #5 allow pods on master--------------------------
+if [[ $1 == '1' ]]; then
 sudo kubectl taint nodes --all node-role.kubernetes.io/master-
-
+fi
 
