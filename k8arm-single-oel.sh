c7e7ef14530ca5dea0ee1e9c680ea6605c30eec4
Anderson de Almeida <70177495+andersonalmeida1@users.noreply.github.com>
2021-06-10 17:54:16 -0300

    Update k8arm-single-oel.sh

--- k8arm-single-oel.sh
+++ k8arm-single-oel.sh
@@ -40,12 +40,13 @@ echo "122222222222222222222222222222222222222222222222222222222222222222221"
 sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
-baseurl=http://yum.kubernetes.io/repos/kubernetes-el7-aarch64
+#baseurl=http://yum.kubernetes.io/repos/kubernetes-el7-aarch64
+#baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
 enabled=1
 gpgcheck=1
 repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
-        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
 sudo cat <<EOF >  /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
